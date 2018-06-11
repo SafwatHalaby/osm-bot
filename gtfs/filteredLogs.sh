a14090b557950cefb5e738db1c96a56f055a4682
SafwatHalaby <5030488+SafwatHalaby@users.noreply.github.com>
2018-06-11 07:12:44 +0300

    typo

--- gtfs/filteredLogs.sh
+++ gtfs/filteredLogs.sh
@@ -12,5 +12,5 @@ suppress=`echo "$suppress" | sort`
 # Only print lines that are in latest.txt but not in suppress.txt
 comm -23 <(echo "$latest") <(echo "$suppress")
 
-# Remove lines that are only in supress.txt from suppress.txt
+# Remove lines that are only in suppress.txt from suppress.txt
 comm -12 <(echo "$latest") <(echo "$suppress") > suppress.txt
